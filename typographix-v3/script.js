ddfe5e2ba1f11840e2b07f3a6e50da3dd8a6731e
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-03-21 19:58:01 +0200

    typographix-v3 start working on hamburger menu

--- typographix-v3/script.js
+++ typographix-v3/script.js
@@ -45,3 +45,17 @@ if (currentThemeFromLocalStorage) {
         lightkMode;
     }
 }
+
+//Navigation
+const nav = document.querySelector("#nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+const toggleMenu = () => {
+    nav.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+};
+
+const hideMenu = () => {
+    nav.classList.remove("active");
+    menuIcon.classList.remove("active");
+};
