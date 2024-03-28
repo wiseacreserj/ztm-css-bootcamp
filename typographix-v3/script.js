8c78bba4ad96db5f2536e4caaa3668f270822289
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-03-28 23:26:37 +0200

    typographix some rename fixes

--- typographix-v3/script.js
+++ typographix-v3/script.js
@@ -1,26 +1,61 @@
-//Function to check if page is scrolled and adjust the logo size
+const themeSwitcher = document.querySelector("#theme-switcher");
 
-const checkScroll = () => {
-    const navbar = document.getElementById("navbar");
-    const logo = document.getElementById("logo");
-    let scrollPosition = window.scrollY;
-
-    // Add/Remove scrolled class based on scrollPositon
-    if (scrollPosition > 20) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-
-    //Calculate new font-size based on scrollPosition
-
-    let newSize = 3 - scrollPosition * 0.03; // Decrease by 0.03rem for every 1px scrolled
-    //Clamping the font size between 1.5rem and 3rem
-    newSize = Math.max(1.5, newSize);
-    newSize = Math.min(3, newSize);
-    logo.style.fontSize = newSize + "rem";
+/* Dark Mode Styles */
+const darkMode = () => {
+    themeSwitcher.children[0].textContent = "Dark Mode";
+    themeSwitcher.children[1].classList.replace("fa-sun", "fa-moon");
 };
 
-//Event Listener for scroll event
+/* Light Mode Styles */
+const lightkMode = () => {
+    themeSwitcher.children[0].textContent = "Light Mode";
+    themeSwitcher.children[1].classList.replace("fa-moon", "fa-sun");
+};
 
-window.addEventListener("scroll", checkScroll);
+/* Switch Theme */
+
+const switchTheme = () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    if (!currentTheme || currentTheme === "light") {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+        darkMode();
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+        lightkMode();
+    }
+};
+
+//Event Listeners
+
+themeSwitcher.addEventListener("click", switchTheme);
+
+//Check Local Storage for Theme
+
+const currentThemeFromLocalStorage = localStorage.getItem("theme");
+if (currentThemeFromLocalStorage) {
+    document.documentElement.setAttribute(
+        "data-theme",
+        currentThemeFromLocalStorage
+    );
+    if (currentThemeFromLocalStorage === "dark") {
+        darkMode();
+    } else {
+        lightkMode;
+    }
+}
+
+//Navigation
+const nav = document.querySelector("#nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+const toggleMenu = () => {
+    nav.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+};
+
+const hideMenu = () => {
+    nav.classList.remove("active");
+    menuIcon.classList.remove("active");
+};
