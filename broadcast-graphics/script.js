1c9f15e8a62d7c38f2b93a0ae31a1d665bb614f7
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-04-08 05:25:36 +0300

    broadcast-graphics JS code for animations, project finished

--- broadcast-graphics/script.js
+++ broadcast-graphics/script.js
@@ -1,8 +1,51 @@
 const goldBar = document.querySelector(".gold-bar");
 const blackBar = document.querySelector(".black-bar");
 const bottomBar = document.querySelector(".bottom-bar");
+const topBar = document.querySelector(".top-bar");
 const textLayer = document.querySelector(".main-bar-text");
 const bottomBarText = document.querySelector(".bottom-bar .bottom-bar-text");
+const logo = document.querySelector(".img-container");
+
+const runAnimationIN = () => {
+    //Re-run animations
+    logo.style.animation = "revealLeft 0.5s linear forwards";
+    goldBar.style.animation = "slideIn 0.5s ease-in-out forwards 0.1s";
+    blackBar.style.animation = "slideIn 0.5s ease-in-out forwards 0.1s";
+    bottomBar.style.animation =
+        "slideDownBottom 0.6s ease-in-out 0.2s forwards";
+    topBar.style.animation = "slideUpTop 0.5s ease-in forwards 0.2s";
+};
+
+const runAnimationOut = () => {
+    //Set clip path to match end of animation in
+    goldBar.style.clipPath = "polygon(15px 0, 100% 0, 100% 100%, 0% 100%)";
+    blackBar.style.clipPath = "polygon(15px 0, 100% 0, 100% 100%, 0% 100%)";
+
+    //Re-run animations
+    logo.style.animation = "hideLeft 0.5s linear forwards";
+    goldBar.style.animation = "slideOut 0.4s ease-in-out forwards 0.3s";
+    blackBar.style.animation = "slideOut 0.4s ease-in-out forwards 0.2s";
+    bottomBar.style.animation = "slideUpBottom 0.3s ease-in-out  forwards";
+    topBar.style.animation = "slideDownTop 0.3s ease-in forwards ";
+};
+
+//Keyboard Shortcut to Toggle Animations
+
+let isGraphicVisible = true;
+
+window.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    //Check if the pressed key code is "Space" for spacebar
+    if (event.code === "Space") {
+        if (isGraphicVisible) {
+            runAnimationOut();
+            isGraphicVisible = false;
+        } else {
+            location.reload();
+            isGraphicVisible = true;
+        }
+    }
+});
 
 //Adjust bar width to fit text
 
