4d6b58dcfe1d6786bd498d4da29c996ddb730c2c
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-04-09 14:07:40 +0300

    developer-portfolio add images, slideshow

--- developer-portfolio/script.js
+++ developer-portfolio/script.js
@@ -12,3 +12,15 @@ const hideMenu = () => {
     menuIcon.classList.remove("active");
 };
  */
+
+//Slideshow------------------------------------------------
+let currentImageIndex = 0;
+const images = document.querySelectorAll(".slide");
+
+const switchImage = () => {
+    images[currentImageIndex].classList.remove("active");
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].classList.add("active");
+};
+
+setInterval(switchImage, 5000);
