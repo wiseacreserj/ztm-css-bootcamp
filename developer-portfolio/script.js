e14ffa22c171812fe20c3be12df02b1767229fcd
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-04-09 13:34:55 +0300

    developer-portfolio initial commit

---
+++ developer-portfolio/script.js
@@ -0,0 +1,14 @@
+//Navigation ----------------------------------------
+/* const nav = document.querySelector("#nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+const toggleMenu = () => {
+    nav.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+};
+
+const hideMenu = () => {
+    nav.classList.remove("active");
+    menuIcon.classList.remove("active");
+};
+ */
