84078348019a7dccd620ac3581ac9bafdbcc984a
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-05-17 19:44:40 +0300

    typographix-final  finished contact section

--- typographix-final/script.js
+++ typographix-final/script.js
@@ -45,3 +45,34 @@ const hideMenu = () => {
     nav.classList.remove("active");
     menuIcon.classList.remove("active");
 };
+
+//Form Submition -------------------------------------------
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const form = this;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: "POST",
+        body: formData,
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                form.reset();
+                const toast = document.getElementById("toast");
+                toast.classList.add("show");
+                setTimeout(() => {
+                    toast.classList.remove("show");
+                }, 10000);
+            } else {
+                //Handle error
+
+                alert("Form sumbission failed");
+            }
+        })
+        .catch((error) => console.error("Error", error));
+});
